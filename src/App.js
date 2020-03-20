b0d2177dbfbf8bcaabcd04ca92e1a1e732d61f72
Harvey Peachey <harvey.peachey@bbc.co.uk>
2020-03-20 13:25:55 +0000

    Amended data export and added some pokemon in the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import pokemon from './data/pokemon.min.js'
 
 function App() {
+  console.log(pokemon);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{pokemon[5].name}</h2>
+      <img src={pokemon[5].img} alt={pokemon[5].name} />
+      <h2>{pokemon[434].name}</h2>
+      <img src={pokemon[434].img} alt={pokemon[434].name} />
     </div>
   );
 }
