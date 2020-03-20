5384b839a170a441ccc7400b2ed7ead9f4d7e464
Harvey Peachey <harvey.peachey@bbc.co.uk>
2020-03-20 15:57:00 +0000

    Added support for gmax pokemon

--- preprocessor/preprocessor.js
+++ preprocessor/preprocessor.js
@@ -1,12 +1,18 @@
 import { Pokemon } from './pokemon.js';
 import { CatchRate } from './catchRate.js';
+import { GmaxPokemon } from './gmaxPokemon.js';
 
 let reformattedPokemon = Pokemon.map((obj) => {
-  obj.catch_rate = CatchRate[obj.id-1];
   if (obj.id > 890) {
-    return;
+    return undefined;
   }
+  obj.catch_rate = CatchRate[obj.id-1];
   obj.img = `https://www.pkparaiso.com/imagenes/espada_escudo/sprites/animados-gigante/${(obj.name).toLowerCase()}.gif`.replace(' ','_');
+  obj.gmax = false;
+  if (obj.id === GmaxPokemon.find(id => obj.id)) {
+    obj.gmax = true;
+    obj.img_gmax = `https://www.pkparaiso.com/imagenes/espada_escudo/sprites/animados-gigante/${(obj.name).toLowerCase()}-gigantamax.gif`.replace(' ','_');
+  }
   return obj;
 })
 
@@ -15,4 +21,4 @@ reformattedPokemon = reformattedPokemon.filter((i) => i);
 var json = JSON.stringify(reformattedPokemon);
 
 var fs = require('fs');
-fs.writeFile('../data/pokemon.min.json', json, 'utf8', () => {});
+fs.writeFile('./processedData/pokemon.min.json', json, 'utf8', () => {});
