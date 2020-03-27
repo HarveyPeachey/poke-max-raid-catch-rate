cdfbc8d7c77866dc37df32244ac5a4a83b5e8664
Harvey Peachey <harvey.peachey@bbc.co.uk>
2020-03-27 15:29:13 +0000

    Added mapping and onchange handling for pokemon

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import React from 'react';
+import React, { useState, useCallback }  from 'react';
 import './App.css';
 import pokemon from './data/pokemon.min.js'
 
 function App() {
-  console.log(pokemon);
+  const [pokemonId, setPokemonId] = useState(0);
   const optionItems = pokemon.map((item) =>
     <option key={item.name}>{item.name}</option>
   );
+  const handleChange = useCallback(e => {
+    setPokemonId(e.target.selectedIndex);
+  }, [])
+  const isGmax = pokemon[pokemonId].img_gmax;
   return (
     <div className="App">
       <div>
-        <select>
+        <select onChange={handleChange}>
           {optionItems}
         </select>
       </div>
-      <h2>{pokemon[5].name}</h2>
-      <img src={pokemon[5].img} alt={pokemon[5].name} />
-      <img src={pokemon[5].img_gmax} alt={pokemon[5].name} />
-      <div><p>{pokemon[5].ball[0].name} Catch Rate: {pokemon[5].ball[0].catch_percent}</p></div>
-      <div><p>{pokemon[5].ball[1].name} Catch Rate: {pokemon[5].ball[1].catch_percent}</p></div>
-      <div><p>{pokemon[5].ball[2].name} Catch Rate: {pokemon[5].ball[2].catch_percent}</p></div>
-      <h2>{pokemon[434].name}</h2>
-      <img src={pokemon[434].img} alt={pokemon[434].name} />
-      <div><p>{pokemon[434].ball[0].name} Catch Rate: {pokemon[434].ball[0].catch_percent}</p></div>
-      <div><p>{pokemon[434].ball[1].name} Catch Rate: {pokemon[434].ball[1].catch_percent}</p></div>
-      <div><p>{pokemon[434].ball[2].name} Catch Rate: {pokemon[434].ball[2].catch_percent}</p></div>
+      <h2>{pokemon[pokemonId].name}</h2>
+      <div class='pokemon-images'>
+        <img src={pokemon[pokemonId].img} alt={pokemon[pokemonId].name} />
+        {isGmax && <img src={pokemon[pokemonId].img_gmax} alt={pokemon[pokemonId].name} />}
+      </div>
+      <div><strong><p>{pokemon[pokemonId].ball[0].name} Catch Rate: {pokemon[pokemonId].ball[0].catch_percent}</p></strong></div>
+      <div><strong><p>{pokemon[pokemonId].ball[1].name} Catch Rate: {pokemon[pokemonId].ball[1].catch_percent}</p></strong></div>
+      <div><strong><p>{pokemon[pokemonId].ball[2].name} Catch Rate: {pokemon[pokemonId].ball[2].catch_percent}</p></strong></div>
     </div>
   );
 }
